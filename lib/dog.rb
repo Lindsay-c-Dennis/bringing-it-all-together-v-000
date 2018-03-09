13c522538ff4bfe94630f4a5efedd4f4671cc407
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-09 23:02:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,10 +47,19 @@ class Dog
   
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM dogs WHERE name = ?
+      SELECT * FROM dogs WHERE name = ?
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row) 
+    end.first 
+  end
+  
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
     end.first 
   end
 end 
